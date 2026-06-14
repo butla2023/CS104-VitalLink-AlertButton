f7d636b16e9adfc9814c563fbd28a8d681572f00
Lumang But <lovelybrotherbut@gmail.com>
2026-06-14 00:46:53 -0600

    init commit

--- alert_button.py
+++ alert_button.py
@@ -1,5 +1,6 @@
 import time
 import RPi.GPIO as GPIO
+import requests
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -7,7 +8,8 @@ button_pressed = False
 while True:
     if GPIO.input(7) == GPIO.HIGH and not button_pressed:
         print("Someone pressed the alert button!")
-        button_pressed = True
+        response = requests.post("https://api.telegram.org/bot8854692912:AAGM9bI6eLRCvYIlFgTVRG7HHqgqLvr1Wxc/sendMessage", json={"chat_id": "6611321117", "text": "Someone pressed the alert button!"})
+
     elif GPIO.input(7) == GPIO.LOW:
-        button_pressed = False
+    	button_pressed = False
     time.sleep(0.1)
